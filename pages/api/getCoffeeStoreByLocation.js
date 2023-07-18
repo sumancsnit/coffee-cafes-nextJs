d894e54e7d05276f555e1d2fcc5ac7f927a310a5
Suman Kumar <sumancsnit@gmail.com>
2023-07-18 22:42:58 +0530

    feat: backend api setup

---
+++ pages/api/getCoffeeStoreByLocation.js
@@ -0,0 +1,16 @@
+import { fetchCoffeeStores } from './../../lib/coffee-store';
+
+const getCoffeeStoresByLocation = async (req, res) => {
+  const { lat, lon } = req.query;
+  try {
+    const response = await fetchCoffeeStores(lat, lon);
+    res.status(200);
+    res.json(response);
+  } catch (error) {
+    console.error('There is an error', error);
+    res.status(500);
+    res.json({ message: 'Something went wrong', error });
+  }
+};
+
+export default getCoffeeStoresByLocation;
